506ecb7984144441b8ac89e2dcb5a25406d554a7
krzysdz <krzysdz@users.noreply.github.com>
2026-08-09 11:40:52 +0200

    don't show false

--- statusboard/template/js/project-list.js
+++ statusboard/template/js/project-list.js
@@ -58,14 +58,14 @@ class ProjectList extends LitElement {
                 </a>
               </td>
               <td>
-                ${project.packageJson && !project.packageJson.private && (html`
+                ${project.packageJson && (!project.packageJson.private || undefined) && (html`
                   <a href="https://npmjs.org/package/${project.packageName}">
                     <img src="https://badgen.net/npm/v/${project.packageName}" />
                   </a>
                 `)}
               </td>
               <td>
-                ${project.packageJson && !project.packageJson.private && (html`
+                ${project.packageJson && (!project.packageJson.private || undefined) && (html`
                   <a href="https://npmjs.org/package/${project.packageName}">
                     <img src="https://badgen.net/npm/dm/${project.packageName}" />
                   </a>
